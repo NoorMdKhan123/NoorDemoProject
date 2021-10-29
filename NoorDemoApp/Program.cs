816203987e8c84ffa9317114f18ceefb03f3d390
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-29 19:30:34 +0530

    Data Inserstion

---
+++ NoorDemoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NoorDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void Memo()
+        {
+            Console.WriteLine("Demo");
+        }
+    }
+}
